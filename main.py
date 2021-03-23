42dd39c6653d453df40cd475042d3c7521f64d16
tbrek <me@tombrek.com>
2021-03-23 21:49:47 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from xbmcswift2 import Plugin, ListItem
-from html.Parser import HTMLParser
+from html.parser import HTMLParser
 from collections import namedtuple
 from datetime import datetime, timedelta, tzinfo
 from language import get_string as _
